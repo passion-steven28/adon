425156269f5b376e964c86455c27ee40520f3777
passion studio <120102511+passion-steven28@users.noreply.github.com>
2024-12-07 12:08:36 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NavBar from './components/shared/nav-bar'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <main className="flex flex-col items-center justify-center min-h-screen">
+
+      </main>
+    </>
+  )
+}
+
+export default App
