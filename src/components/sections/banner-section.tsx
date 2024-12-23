f62d481ad74d76cbd8362a2fca2308c3559214ce
passion studio <120102511+passion-steven28@users.noreply.github.com>
2024-12-23 18:42:28 +0300

    add new SVG assets and refactor banner image source in BannerSection component

--- src/components/sections/banner-section.tsx
+++ src/components/sections/banner-section.tsx
@@ -1,10 +1,12 @@
+import banner from '../../assets/img/banner.jpg'
+
 
 const BannerSection = () => {
     return (
         <div className="w-full h-[40vh]">
             <img
-                src="../../../public/images/banner.jpg"
-                alt="banner-image"
+                src={banner}
+                alt="banner image"
                 className="w-full h-full object-cover relative"
             />
         </div>
